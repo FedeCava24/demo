579dab0ea0f5d50fab8bf5a4bfb70d28ceb9db42
FedeCava24 <cavagnari.federico@gmail.com>
2023-03-28 12:13:03 +0200

    modified

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -11,12 +12,19 @@ import java.awt.*;
 
 @Route
 
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
     public MainView() {
         var button = new Button("Click me!");
         var textField = new TextField();
 
-        add(textField, button);
+
+
+        add(new HorizontalLayout(textField, button));
+
+        button.addClickListener(e -> {
+            add(new Paragraph("Hello, " + textField.getValue()));
+            textField.clear();
+        });
 
 
 

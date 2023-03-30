5c6c800a1873123b196bdc9ce57b54869a6daa1f
Lucaliga00 <Luca.liga327@gmail.com>
2023-03-30 18:01:18 +0200

    aggiunte data e email

--- src/main/java/com/example/demo/RegistrationPage.java
+++ src/main/java/com/example/demo/RegistrationPage.java
@@ -11,7 +11,11 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.router.Route;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Route("registration")
 public class RegistrationPage extends VerticalLayout {
@@ -19,16 +23,18 @@ public class RegistrationPage extends VerticalLayout {
         TextField firstNameField = new TextField("First name", "", "");
         TextField lastNameField = new TextField("Last name", "", "");
         TextField username = new TextField("Username");
+        EmailField emailField = new EmailField("E-mail");
+        DatePicker dateBirth = new DatePicker("Data di nascita");
         PasswordField password = new PasswordField("Password");
         PasswordField confirmPassword = new PasswordField("Confirm password");
         ComboBox<String> incarichi = new ComboBox<>("Inacarichi");
 
         incarichi.setAllowCustomValue(true);
         //Aggiungere i vari tipi di incarichi
-        incarichi.setItems("Incarico1", "Incarico2", "Incarico3");
+        incarichi.setItems("PR", "Barista", "Cambusiere", "gestore");
         incarichi.setHelperText("Seleziona o scrivi l'incarico svolto");
 
-        FormLayout formLayout = new FormLayout(firstNameField, lastNameField, username,
+        FormLayout formLayout = new FormLayout(firstNameField, lastNameField, username, emailField, dateBirth,
                 password, confirmPassword, incarichi);
         formLayout.setResponsiveSteps(new ResponsiveStep("0", 2));
 
@@ -39,7 +45,9 @@ public class RegistrationPage extends VerticalLayout {
                 username.getValue(),
                 password.getValue(),
                 confirmPassword.getValue(),
-                incarichi.getValue())
+                incarichi.getValue(),
+                emailField.getValue(),
+                String.valueOf(dateBirth.getValue()))
 
         );
         createAccount.addClickListener(e -> {
@@ -59,8 +67,8 @@ public class RegistrationPage extends VerticalLayout {
 
 
 
-    private void register (String  firstname, String lastname ,String username,String password, String confirmPassword,
-                              String incarichi ) {
+    private void register (String  firstname, String lastname , String username, String email, String dateBirth, String password, String confirmPassword,
+                           String incarichi ) {
         if (username.trim().isEmpty()) {
 
             Notification.show("Enter a username");
@@ -88,6 +96,12 @@ public class RegistrationPage extends VerticalLayout {
 
             Notification.show("Select a incarico");
 
+        }
+        else if (email.isEmpty()) {
+            Notification.show("Enter a E-mail");
+        }
+        else if (dateBirth.isEmpty()) {
+            Notification.show("Enter the Birthday's date");
         }
 
     }

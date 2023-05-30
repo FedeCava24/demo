090df76506e9403a4c95b4b74cd6dd74cab87d2c
Lucaliga00 <Luca.liga327@gmail.com>
2023-05-30 16:38:07 +0200

    Modificato il metodo di creazione dell'ordine, prova di stampa e risulta corretto il procedimento di salvataggio dell'ordine, rimane da collegare il tutto ai pannelli

--- src/main/java/com/example/demo/OrdersPage.java
+++ src/main/java/com/example/demo/OrdersPage.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -20,7 +19,6 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 
 import javax.swing.*;
-import java.awt.*;
 
 
 @Route("Orders")
@@ -30,8 +28,14 @@ public class OrdersPage extends AppLayout {
     String []Nome= {"Prosecco","Bellavista","Moet","Moet Ice","Moet Ice Rose","Dom Perignon","Dom Perignon Rose", "Dom Perigno Luminous","Dom Perignon Magnum","Cristal","Krug","Armand de Brignac","Armand de Brignac Rose","Armand de Brignac verde","Armand de Brignac Magnum","Bombay","Bombay Magnum","Havana","Below42","Grey Goose","Grey Goose Magnum","Grey Goose Jereboam","Grey Goose Mathusalem","Belvedere Magnum Esseti","Hendricks","Gin Mare","Malfi Pompelmo","Malfi Limone","Malfi Arancia"};
     Integer[]Quantita={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     String NumTavolo ="null";
-    VerticalLayout verticalLayoutpanel=new VerticalLayout();
 
+    Integer[][] arrayPrezzo=new Integer[150][29];
+
+    String[][] arrayNome= new String[150][29];
+    Integer[][] arrayQuantita=new Integer[150][29];
+    String[][] arrayNumTavolo= new String[150][1];
+    private JFrame frame ;
+    private JPanel mainpanel;
     public OrdersPage(){
 
 
@@ -127,7 +131,7 @@ public class OrdersPage extends AppLayout {
     private TabSheet getSecondaryNavigation(){
         TabSheet tabs =new TabSheet();
         tabs.add("New order Friday", new newOrderFriday());
-        tabs.add("Open",new openOrder(Nome, Quantita,Prezzo,NumTavolo));
+        tabs.add("Open",new openOrder());
        /* tabs.add("Completed",new completedOrder());*/
         tabs.setHeight("720px");
         tabs.setWidth("1180px");
@@ -381,12 +385,12 @@ public class OrdersPage extends AppLayout {
             Label priceBelvedereMagnumEsseti =new Label("550");
             addFormItem(priceBelvedereMagnumEsseti,"PRICE");
 
-            IntegerField hendricksFiles = new IntegerField();
-            hendricksFiles.setValue(0);
-            hendricksFiles.setStepButtonsVisible(true);
-            hendricksFiles.setMin(0);
-            hendricksFiles.setMax(999);
-            addFormItem(hendricksFiles, "Hendricks");
+            IntegerField hendricksField = new IntegerField();
+            hendricksField.setValue(0);
+            hendricksField.setStepButtonsVisible(true);
+            hendricksField.setMin(0);
+            hendricksField.setMax(999);
+            addFormItem(hendricksField, "Hendricks");
 
             Label priceHndrick =new Label("220");
             addFormItem(priceHndrick,"PRICE");
@@ -437,13 +441,43 @@ public class OrdersPage extends AppLayout {
             tableNumber.setItems( "C1","C2","C3","C4","C5","C6","C7","C8","C9","P1","P2","P3","P4","P5","P6","P7","P8","P9","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","A1","A2","A3","A4","A5","A6","A7","A8","A9","G1","G2","G3","G4","G5");
             addFormItem(tableNumber,"");
 
-            Button sendOrderFriday=new Button("Send Order");
-            addFormItem(sendOrderFriday,"");
+            Button sendOrder=new Button("Send Order");
+            addFormItem(sendOrder,"");
 
 
 
-            sendOrderFriday.addClickListener(e-> new createOrder(proseccoField.getValue(),bellavistaField.getValue(),moetField.getValue(),moeticeField.getValue(),moeticeRField.getValue(),domField.getValue(),domroseField.getValue(),domluminouseField.getValue(),domMagnumField.getValue(),cristalField.getValue(),krugField.getValue(),armandField.getValue(),armandroseField.getValue(),armandverdeField.getValue(),armandmagnumField.getValue(),bombayField.getValue(),bombaymagnumField.getValue(),havanaField.getValue(),belowField.getValue(),greyField.getValue(),greyMagnumField.getValue(),greyJereboamField.getValue(),greyMathusalemField.getValue(),belvederemagnumField.getValue(),hendricksFiles.getValue(),mareField.getValue(),malfiPField.getValue(),malfiLField.getValue(),malfiAField.getValue(),tableNumber.getValue()));
-
+            sendOrder.addClickListener(e-> new setOrder(proseccoField.getValue(),bellavistaField.getValue(),moetField.getValue(),moeticeField.getValue(),moeticeRField.getValue(),domField.getValue(),domroseField.getValue(),domluminouseField.getValue(),domMagnumField.getValue(),cristalField.getValue(),krugField.getValue(),armandField.getValue(),armandroseField.getValue(),armandverdeField.getValue(),armandmagnumField.getValue(),bombayField.getValue(),bombaymagnumField.getValue(),havanaField.getValue(),belowField.getValue(),greyField.getValue(),greyMagnumField.getValue(),greyJereboamField.getValue(),greyMathusalemField.getValue(),belvederemagnumField.getValue(),hendricksField.getValue(),mareField.getValue(),malfiPField.getValue(),malfiLField.getValue(),malfiAField.getValue(),tableNumber.getValue()));
+            sendOrder.addClickListener(e-> {
+                proseccoField.setValue(0);
+                bellavistaField.setValue(0);
+                moetField.setValue(0);
+                moeticeField.setValue(0);
+                moeticeRField.setValue(0);
+                domField.setValue(0);
+                domroseField.setValue(0);
+                domluminouseField.setValue(0);
+                domMagnumField.setValue(0);
+                cristalField.setValue(0);
+                krugField.setValue(0);
+                armandField.setValue(0);
+                armandroseField.setValue(0);
+                armandverdeField.setValue(0);
+                armandmagnumField.setValue(0);
+                bombayField.setValue(0);
+                bombaymagnumField.setValue(0);
+                havanaField.setValue(0);
+                belowField.setValue(0);
+                greyField.setValue(0);
+                greyMagnumField.setValue(0);
+                greyJereboamField.setValue(0);
+                greyMathusalemField.setValue(0);
+                belvederemagnumField.setValue(0);
+                hendricksField.setValue(0);
+                mareField.setValue(0);
+                malfiPField.setValue(0);
+                malfiLField.setValue(0);
+                malfiAField.setValue(0);
+            });
 
         }
 
@@ -451,61 +485,67 @@ public class OrdersPage extends AppLayout {
     }
 
     public  class openOrder extends Div {
-        VerticalLayout verticalLayoutpanel=new VerticalLayout();
-        Integer[][] Prezzo=new Integer[150][28];
+        public openOrder() {
+          /*  frame = new JFrame("Pannelli ordine");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        String[][] Nome= new String[150][28];
-        Integer[][] Quantita=new Integer[150][28];
-        String[][] NumTavolo= new String[150][1];
+            mainpanel = new JPanel();
+            mainpanel.setLayout(new BoxLayout(mainpanel, BoxLayout.Y_AXIS));
+            for(int i=0;i<=150;i++){
+                if(arrayNumTavolo[i][0]!=null){
+                    JPanel orderPanel=new JPanel();
+                    orderPanel.setLayout(new BoxLayout(orderPanel,BoxLayout.Y_AXIS));
+                    JLabel titleLabel=new JLabel(arrayNumTavolo[i][0]);
+                    orderPanel.add(titleLabel, BorderLayout.NORTH);
+                    Integer totale=0;
+                    for(int j=0;j<=28;j++){
+                        if(arrayQuantita[i][j]!=null){
+                            JLabel txt=new JLabel(arrayNome[i][j]);
+                            totale+=arrayQuantita[i][j]*arrayPrezzo[i][j];
+                            orderPanel.add(txt,arrayQuantita[i][j]);
 
+                        }
+                    }
+                    JTextField bill=new JTextField(totale);
+                    bill.setEditable(false);
+                    orderPanel.add("Totale Ordine",bill);
+                    mainpanel.add(orderPanel);
+                }
 
-        public openOrder(String[] newNome, Integer[]newQuantita,Integer[] newPrezzo,String newNumTavolo){
-           for(int i=0;i<=150;i++){
-               if(NumTavolo[i][0]==null){
-                   NumTavolo[i][0]=new String(newNumTavolo);
-                   for (int j=0;j<=28;j++){
-                       Prezzo[i][j]=newPrezzo[j];
-                       Quantita[i][j]=newQuantita[j];
-                       Nome[i][j]= new String(newNome[j]);
-                   }
-                   JPanel newPanel=new JPanel();
-                   Integer totale=0;
-                   for(int j=0;j<=28;j++){
-                       if(Quantita[i][j]!=0){
-                           JLabel label=new JLabel(Nome[i][j]);
-                           newPanel.add(label,Quantita[i][j]);
-                           totale+=Quantita[i][j]*Prezzo[i][j];
-                       }
-                   }
-                   JLabel totaleLabel=new JLabel(String.valueOf(totale));
-                   newPanel.add("TOTALE ORDINE",totaleLabel);
-                   Button completed=new Button("Completato");
-
-                   newPanel.getAlignmentY();
-                   newPanel.setBackground(Color.CYAN);
-                   completed.addClickListener(e->{
-                       completed.setVisible(false);
-                   });
-
-                   verticalLayoutpanel.add(newPanel);
-                   break;
+            }
 
 
-               }
-           }
-            add(verticalLayoutpanel);
+
+
+            JScrollPane scrollPane = new JScrollPane(mainpanel);
+            frame.getContentPane().add(scrollPane);
+
+            frame.pack();
+            frame.setVisible(true);
+*/
+
+            //prove di stampa ordini riuscite correttamente
+            Button print = new Button("stampa");
+            print.addClickListener(e -> {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println(arrayNumTavolo[i][0]);
+                    Integer totale=0;
+                    for(int j=0;j<=28;j++){
+                        if(arrayQuantita[i][j]!=null){
+                            System.out.println(arrayNome[i][j] + " " + arrayQuantita[i][j]);
+                            totale+=arrayPrezzo[i][j]*arrayQuantita[i][j];
+                        }
+                    }
+                    System.out.println("Totale Ordine: "+ totale+"€");
+                }
+            });
+
+            add(print);
         }
     }
+    public  class setOrder  {
 
-    public  class completedOrder extends JFrame{
-        public completedOrder(){
-
-
-
-        }
-    }
-    public  class createOrder  {
-        public createOrder(Integer prosecco,Integer bellavista,Integer moet,Integer moetIce,Integer moetIceRose,Integer dom,Integer domRose,Integer domLumi,Integer domM, Integer cristal,Integer krug,Integer armand,Integer armandRose,Integer armandVerde ,Integer armandMagnum,Integer bombay,Integer bombayMagnum,Integer havana,Integer below,Integer grey,Integer greyMagnum,Integer greyJ,Integer greyMat,Integer belvedereM,Integer hendricks,Integer mare,Integer malfiP,Integer malfiL,Integer malfiA, String numTavolo) {
+        public setOrder(Integer prosecco,Integer bellavista,Integer moet,Integer moetIce,Integer moetIceRose,Integer dom,Integer domRose,Integer domLumi,Integer domM, Integer cristal,Integer krug,Integer armand,Integer armandRose,Integer armandVerde ,Integer armandMagnum,Integer bombay,Integer bombayMagnum,Integer havana,Integer below,Integer grey,Integer greyMagnum,Integer greyJ,Integer greyMat,Integer belvedereM,Integer hendricks,Integer mare,Integer malfiP,Integer malfiL,Integer malfiA, String numTavolo) {
             Quantita[0] = prosecco;
             Quantita[1] = bellavista;
             Quantita[2] = moet;
@@ -535,11 +575,28 @@ public class OrdersPage extends AppLayout {
             Quantita[26] = malfiP;
             Quantita[27] = malfiL;
             Quantita[28] = malfiA;
-            new openOrder(Nome,Quantita,Prezzo,numTavolo);
+            createOrder(Nome,Quantita,Prezzo,numTavolo);
         }
-    }
+
+
+        public void createOrder (String[] newNome, Integer[]newQuantita,Integer[] newPrezzo,String newNumTavolo){
+            for (int i=0;i<=150;i++){
+                if(arrayNumTavolo[i][0]==null){
+                    arrayNumTavolo[i][0]=newNumTavolo;
+                    for (int j=0;j<=28;j++){
+                        if(newQuantita[j]!=0){
+                            arrayNome[i][j]=newNome[j];
+                            arrayQuantita[i][j]=newQuantita[j];
+                            arrayPrezzo[i][j]=newPrezzo[j];
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }}
 
 
 
 
-}
+
